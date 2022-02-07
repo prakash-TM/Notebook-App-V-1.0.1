5648f2ae4b07d9e69aeefe310ea1dc05bef2dc2c
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2022-02-07 11:11:47 +0530

    changed api

--- frontEnd/noteBookAppUI/src/requests/deleteAll.ts
+++ frontEnd/noteBookAppUI/src/requests/deleteAll.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const DeleteAllReq=async (deleteAllApi:any)=>{
     const responseData=await axios
-    .delete(`http://ec2-13-232-172-209.ap-south-1.compute.amazonaws.com:8000/${deleteAllApi}`)
+    .delete(`http://localhost:8000/${deleteAllApi}`)
     return(responseData)
 }
 export default DeleteAllReq
--- frontEnd/noteBookAppUI/src/requests/getAll.ts
+++ frontEnd/noteBookAppUI/src/requests/getAll.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const GetAllReq=async (getAllApi:any)=>{
     const responseData=await axios
-    .get(`http://ec2-13-232-172-209.ap-south-1.compute.amazonaws.com:8000/${getAllApi}`)
+    .get(`http://localhost:8000/${getAllApi}`)
     return(responseData)
 }
 export default GetAllReq
