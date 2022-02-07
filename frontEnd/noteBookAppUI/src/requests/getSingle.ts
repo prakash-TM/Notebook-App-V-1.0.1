fa118d4ca943e60f815a84100f947ddfffa60b4a
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2022-02-07 09:41:06 +0530

    added host name in request folder

--- frontEnd/noteBookAppUI/src/requests/getSingle.ts
+++ frontEnd/noteBookAppUI/src/requests/getSingle.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const GetSingleReq=async (searchWord:any,getSingleApi:any)=>{
     const responseData=await axios
-    .get(`http://localhost:8000/${getSingleApi}`, { params: { title: searchWord } })
+    .get(`http://ec2-13-232-172-209.ap-south-1.compute.amazonaws.com:8000/${getSingleApi}`, { params: { title: searchWord } })
     return(responseData)
 }
 export default GetSingleReq
--- frontEnd/noteBookAppUI/src/requests/post.ts
+++ frontEnd/noteBookAppUI/src/requests/post.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const PostReq=async (data:any,postApi:any)=>{
     await axios
-    .post(`http://localhost:8000/${postApi}`, data)
+    .post(`http://ec2-13-232-172-209.ap-south-1.compute.amazonaws.com:8000/${postApi}`, data)
 }
 
 export default PostReq
